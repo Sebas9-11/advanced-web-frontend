76031cb85b2f943b24f5a5d2f9a08a48aae14c23
Sebas9-11 <sebastian-1999@outlook.com>
2023-11-23 22:54:22 -0500

    deployed 5

--- src/components/pages/production/production.tsx
+++ src/components/pages/production/production.tsx
@@ -1,5 +1,5 @@
 import pdfMake from 'pdfmake/build/pdfmake'
-import pdfFonts from 'pdfmake/build/vfs_fonts'
+import * as pdfFonts from 'pdfmake/build/vfs_fonts'
 import { useEffect, useState } from 'react'
 import HomeLayout from '../../../layouts/homeLayout'
 import { production, tableHeader } from '../../../types/types'
