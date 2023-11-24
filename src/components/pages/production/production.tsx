b2d03df97834d96a9bf7a59a4dc2498c88db4aac
Sebas9-11 <sebastian-1999@outlook.com>
2023-11-23 22:26:24 -0500

    pdf generate

--- src/components/pages/production/production.tsx
+++ src/components/pages/production/production.tsx
@@ -1,21 +1,63 @@
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
 import { useEffect, useState } from 'react'
 import HomeLayout from '../../../layouts/homeLayout'
 import { production, tableHeader } from '../../../types/types'
 import Tables from '../../tables'
+import HttpProductionApiService from './../../../APIs/production'
 import styles from './production.module.css'
-import HttpProductionApiService from './../../../APIs/production';
-import pdfMake from 'pdfmake/build/pdfmake'
 
+pdfMake.vfs = pdfFonts.pdfMake.vfs
 
 export default function ProductionPage() {
   const [data, setData] = useState<production[]>([])
   const [header, setHeader] = useState<tableHeader[]>([])
   const httpEmployeeApiService = HttpProductionApiService()
 
+  const today = new Date()
+
+  const generatePdf = (production: production) => {
+    const documentDefinition = {
+      content: [
+        {
+          text: 'Shoes Makers Inc.',
+          fontSize: 16,
+          bold: true,
+        },
+        {
+          text: `empleado: ${production.primer_nombre} ${production.apellido}`,
+          fontSize: 12,
+          margin: 5,
+        },
+        {
+          text: `rol: ${production.nombre_rol}`,
+          fontSize: 12,
+          margin: 5,
+        },
+        {
+          text: `producto: ${production.nombre_producto}`,
+          fontSize: 12,
+          margin: 5,
+        },
+        {
+          text: `cantidad: ${production.cantidad}`,
+          fontSize: 12,
+          margin: 5,
+        },
+        {
+          text: `fecha: ${today.getDate()}/${today.getMonth()}/${today.getFullYear()}`,
+          fontSize: 12,
+          margin: 5,
+        },
+      ],
+    }
+    pdfMake.createPdf(documentDefinition).open()
+  }
+
   const getEmployees = async () => {
     const res = await httpEmployeeApiService.getProduction()
     setData(res.data)
-    
+
     const keys = Object.keys(res.data[0])
     const header: tableHeader[] = keys.map((key) => {
       const formattedKey = key
@@ -36,24 +78,11 @@ export default function ProductionPage() {
     getEmployees()
   }, [])
 
-  const dd = {
-    content: [
-      {
-        style: 'tableExample',
-        table: {
-          widths: ['*'],
-          body: [],
-        },
-      },
-    ],
+  const handleEdit = (production: production) => {
+    generatePdf(production)
   }
 
-
-  const handleEdit = () => {
-    pdfMake.createPdf(dd).open()
-  }
-
-  const handleDelete = (employee: production) => {
+  const handleDelete = () => {
     // httpEmployeeApiService.deleteEmployee(employee?.employee_id as number)
   }
 
@@ -64,8 +93,8 @@ export default function ProductionPage() {
       </header>
       <section>
         <Tables
-          textBtn1='pdf'
-          textBtn2='delete'
+          textBtn1="pdf"
+          textBtn2="delete"
           data={data}
           header={header}
           ActionEdit={handleEdit}
